db7fb73b6fbd8869150836657e7c470583bab735
KhloutSambath <65694832+KhloutSambath@users.noreply.github.com>
2021-08-05 09:48:23 +0700

    Probab

---
+++ data/slide10.js
@@ -0,0 +1,11 @@
+(function(){var loadHandler=window['sl_{557B10A5-F122-40E3-9C86-5DDAE4E6BBEB}'];loadHandler&&loadHandler(9, '<div id="spr0_473e0e6"><div id="spr1_473e0e6" class="kern slide"><img id="img1_473e0e6" src="data/img0.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_473e0e6" class="kern slide"><div id="svg0_473e0e6" style="left:-2px;top:-2px;"><svg width="138" height="60" viewBox="-1.5 -1.5 138 60"><path fill="#5b9bd5" stroke="#41719c" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="8" d="M0.5,5.859 h105.75 v-5.359 l27.563,27.563 l-27.563,27.563 v-5.359 h-105.75 l22.203,-22.203 Z"/></svg></div><div id="spr3_473e0e6" style="left:-0.5px;top:-0.5px;"><div style="width:0px;"><span id="txt0_473e0e6" data-width="68.994141" style="left:29.903px;top:9.722px;">ថ្នាក់ទី៩</span></div></div><div id="spr4_473e0e6" style="left:114px;top:49px;"><img id="img0_473e0e6" src="data/img6.png" width="466" height="447" alt="តាង 𝑥 ជាចំនួនឃ្លីពណ៌ខ្មៅ  \
+15−𝑥 ជាចំនួនឃ្លីពណ៌ស\
+ប្រូបាបចាប់បានឃ្លីពណ៌ខ្មៅគឺ 𝑃= 𝑥 15  (1)\
+ចំនួនករណីអាច ជាពិសោធន៍ 100 ដង\
+ចំនួនករណីស្រប ជាការចាប់បានឃ្លីពណ៌ខ្មៅ 40 ដង\
+ប្រូបា​បចាប់បានឃ្លីពណ៌ខ្មៅគឺ 𝑃= 40 100    (2)\
+តាម (1) និង (2) យើងបាន  𝑥 15 = 40 100 \
+100𝑥=600\
+𝑥= 600 100 =6\
+ឃ្លីពណ៌ស មានចំនួន 15-6=9\
+"/></div><div id="svg2_473e0e6" style="left:719.688px;top:1.219px;"><svg width="243" height="88" viewBox="-1.5 -1.5 243 88"><g fill="none"><path fill="#ed7d31" stroke="#ae5a21" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="8" transform="matrix(-1,0,0,1,239.312515,0)" d="M0.5,0.5 h201.672 v-0 l36.64,41.703 l-36.64,41.703 v-0 h-201.672 l36.64,-41.703 Z"/></g></svg></div><div id="spr5_473e0e6" style="left:721.188px;top:2.719px;"><div style="width:0px;"><span id="txt1_473e0e6" data-width="170.625000" style="left:44.34px;top:17.749px;">ចម្លើយគំរូទី៣</span></div></div></div></div>');})();
